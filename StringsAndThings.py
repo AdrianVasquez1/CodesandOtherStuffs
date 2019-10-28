8d007a1648511c9e43fc224f110590fab517c63f
AdrianVasquez1 <100114395@lubbockisd.net>
2019-10-28 08:57:16 -0500

    6th commit using Strings and things

--- StringsAndThings.py
+++ StringsAndThings.py
@@ -14,13 +14,15 @@ print(fullName)
 # Repetition
 #  Repetition operator: *
 
-print("Hip "*2 + "Hooray!")
+print("Hip " * 2 + "Hooray!")
+
 
 def rowYourBoat():
-    print("Row, "*3 + 'your boat')
+    print("Row, " * 3 + 'your boat')
     print("Gently down the stream")
-    print("Merrily, "*4)
+    print("Merrily, " * 4)
     print("Life is but a dream")
+
 
 rowYourBoat()
 
@@ -72,7 +74,7 @@ print("Centered String: ", new_string)
 
 # The string "center" moves the words or text over a certain amount of characters.
 
-    # ljust         aStr.ljust(w)
+# ljust         aStr.ljust(w)
 
 string = 'cat'
 width = 5
@@ -81,7 +83,7 @@ print(string.ljust(width))
 
 # the string ljust makes the total number of characters equal to the width.
 
-    # rjust         aStr.rjust(w)
+# rjust         aStr.rjust(w)
 
 string = 'dog'
 width = 5
@@ -89,7 +91,7 @@ print(string.rjust(width))
 
 # the string ljust makes the total number of characters equal to the width to the right.
 
-    # upper         aStr.upper()
+# upper         aStr.upper()
 
 string = "this should be uppercase!"
 print(string.upper())
@@ -97,7 +99,7 @@ string = "Th!s Sh0uLd B3 uPp3rCas3!"
 print(string.upper())
 
 # the upper string makes any text in the string uppercase.
-    # lower         aStr.lower()
+# lower         aStr.lower()
 
 string = "THIS SHOULD BE LOWERCASE!"
 print(string.lower())
@@ -105,28 +107,29 @@ string = "Th!s Sh0uLd B3 L0w3rCas3!"
 print(string.lower())
 
 # the lower string makes any text inside of the string lowercase
-    # index         aStr.index(item)
+# index         aStr.index(item)
 
 sentence = 'Python programming is fun.'
 result = sentence.index('is fun')
 print("Substring 'is fun':", result)
+sentence = 'Java programming is fun'
 result = sentence.index('Java')
 print("Substring 'Java':", result)
 
 # The index string uses the substring that is inside of the main string and prints that text.
-    # rindex        aStr.rindex(item)
+# rindex        aStr.rindex(item)
 
 quote = 'Let it be, let it be, let it be'
 
 result = quote.rindex('let it')
-print("Subtstring 'let it':", result)
+print("Substring 'let it':", result)
 result = quote.rindex('small')
 print("Substring 'small ':", result)
 
 # the rindex string is similar to index but if the substring is not found
 # the result comes back as -1.
 
-    # find          aStr.find(item)
+# find          aStr.find(item)
 
 quote = 'Let it be, let it be, let it be'
 result = quote.find('let it')
@@ -135,7 +138,7 @@ result = quote.find('small')
 print("Substring 'small ':", result)
 
 # If the substring exist the result is the index of the first occurrence if not -1.
-    # rfind         aStr.rfind(item)
+# rfind         aStr.rfind(item)
 
 quote = 'Let it be, let it be, let it be'
 result = string.rfind('let it')
@@ -143,14 +146,29 @@ print("Substring 'let it':", result)
 result = quote.rfind('small')
 print("Substring 'small ':", result)
 
-#If substring exists inside the string, it returns the highest index where substring is found.
-#If substring doesn't exist inside the string, it returns -1.
+# If substring exists inside the string, it returns the highest index where substring is found.
+# If substring doesn't exist inside the string, it returns -1.
 
-    # replace       aStr.replace(old, new)
+# replace       aStr.replace(old, new)
 
 song = 'cold, cold heart'
 print(song.replace('cold', 'hurt'))
 song = 'Let it be, let it be, let it be, let it be'
 print(song.replace('let', "don't let", 2))
 
-    # Be sure to include multiple examples of all of them in use
+# Be sure to include multiple examples of all of them in use
+
+# Character Functions
+
+print(ord('B'))
+
+print(chr(97+13))
+
+print(str(12548))
+
+# testing functions from mapper.py
+
+from mapper import *
+
+print(letterToIndex('P'))
+print(indexToLetter(10))
