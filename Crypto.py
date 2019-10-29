feb577f1406c57e720409a5e00f01272f047902f
AdrianVasquez1 <100114395@lubbockisd.net>
2019-10-29 08:43:20 -0500

    Added crypto.py and the transposition encryption function

---
+++ Crypto.py
@@ -0,0 +1,17 @@
+# Transposition Cypher
+
+# original: this _is_a_secret_message_that_i_want_to_transmit
+# encrypted:hsi_ertmsaeta_att_rnmtti_sasce_esg_htiwn_otasi
+
+def scramble2Encrypt(plainText):
+    evenChars = ""
+    oddChars = ""
+    charCount = 0
+    for ch in plainText:
+        if charCount % 2 == 0:
+            evenChars = evenChars + ch
+        else:
+            oddChars = oddChars + ch
+        charCount = charCount + 1
+    cipherText = oddChars + evenChars
+    return cipherText
